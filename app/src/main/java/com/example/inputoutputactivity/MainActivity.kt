4123c3f40d1ed3e533ceb84196a56cf5c758e28f
Mattythelaw <matthewconnor0418@gmail.com>
2026-03-12 16:54:06 +0200

    added variables

---
+++ app/src/main/java/com/example/inputoutputactivity/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.inputoutputactivity
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val clickMeButton = findViewById<Button>(R.id.clickBtn)
+        val welcomeTxt = findViewById<TextView>(R.id.Hellotext)
+        val nameTxtField = findViewById<EditText>(R.id.Nametext)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
